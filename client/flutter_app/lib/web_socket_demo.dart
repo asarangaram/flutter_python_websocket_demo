4dcb25feff66745059e37b9e8a06e0538c72fa7f
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-08-22 19:24:50 +0530

    use shadcn resizable panel to show log view in the right side

--- client/flutter_app/lib/web_socket_demo.dart
+++ client/flutter_app/lib/web_socket_demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 import 'views/log_view.dart';
 import 'providers/server_io.dart';
 
@@ -13,39 +14,61 @@ class WebSocketDemo extends ConsumerWidget {
     final isConnected = ref
         .watch(serverIOProvider)
         .whenOrNull(data: (serverIO) => serverIO.isConnected);
-
+    ShadTheme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text("WebSocket Demo")),
-      body: Column(
+      body: ShadResizablePanelGroup(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 20,
-            children: [
-              ElevatedButton(
-                onPressed: isConnected == null
-                    ? null
-                    : isConnected
-                    ? notifier.disconnectFromServer
-                    : notifier.connect,
-                child: Text(
-                  isConnected == null
-                      ? "Wait"
-                      : isConnected
-                      ? "Disconnect"
-                      : "Connect",
-                ),
-              ),
-
-              ElevatedButton(
+          ShadResizablePanel(
+            id: 0,
+            defaultSize: .7,
+            minSize: .2,
+            child: Center(
+              child: ElevatedButton(
                 onPressed: isConnected == true ? notifier.sendProcess : null,
                 child: const Text("Send Process"),
               ),
-            ],
+            ),
           ),
+          ShadResizablePanel(
+            id: 1,
+            defaultSize: .3,
+            minSize: .1,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black87),
 
-          const Divider(),
-          Expanded(child: LogView()),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: ElevatedButton(
+                        onPressed: isConnected == null
+                            ? null
+                            : isConnected
+                            ? notifier.disconnectFromServer
+                            : notifier.connect,
+                        child: Text(
+                          isConnected == null
+                              ? "Wait"
+                              : isConnected
+                              ? "Disconnect"
+                              : "Connect",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(color: Colors.white, height: 1),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: LogView(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
